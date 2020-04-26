ae6b2abc1a8af63992a7dfcff064287bc8854006
mmdarroch <darrochm@student.unimelb.edu.au>
2020-04-26 19:02:07 +1000

    added comments to venuecontrollers

--- controllers/venueController.js
+++ controllers/venueController.js
@@ -2,3 +2,17 @@ const mongoose = require("mongoose");
 
 // import venue model
 const Venue = mongoose.model("venue")
+
+// function to handle a request to get all users
+
+
+// function to modify user by ID
+
+
+// function to add user
+
+
+// function to get user by id
+
+
+// remember to export the functions
