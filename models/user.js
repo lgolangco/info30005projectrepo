e1af1d71648fd417101274bff0745b32cf38eea0
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-04-27 18:22:38 +0800

    Added mongoose-unique-validator package and made entering userid unique

--- models/user.js
+++ models/user.js
@@ -1,10 +1,26 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
-  id: String,
-  first_name: String,
-  last_name: String
+  id: {
+    type: String,
+    unique: true
+  },
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  }
+
+  email: {
+    type: String
+  }
 });
 
 const User = mongoose.model("user", userSchema, "user");
+
+userSchema.plugin(uniqueValidator);
 module.exports = User;
