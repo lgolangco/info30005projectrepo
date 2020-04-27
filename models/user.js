d85785779bf5d946f5f8e24551f85dc66f77632b
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-04-27 17:45:40 +0800

    Make entering a name required

--- models/user.js
+++ models/user.js
@@ -2,8 +2,17 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   id: String,
-  first_name: String,
-  last_name: String
+
+  first_name: {
+    type: String,
+    required: true
+  },
+
+  last_name: {
+    type: String,
+    required: true
+  }
+
 });
 
 const User = mongoose.model("user", userSchema, "user");
