4c4da64ba207e30dad6e7461926726f2dd41c1ef
Lucy <llskwu@gmail.com>
2020-04-27 17:24:49 +1000

    Update review.js

--- models/review.js
+++ models/review.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema({
     review_id: String,
+    venue_id: String,
     user_id: String,
-    first_name: String,
-    last_name: String,
     review: String
 });
 
