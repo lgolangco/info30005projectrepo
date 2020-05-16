93b455a6dd6e352e564b9ebbfd160353fd00de49
lgolangco <lgolangco@student.unimelb.edu.au>
2020-05-16 18:44:55 +1000

    added user details to the venue suggestions page

---
+++ models/venuesuggestions.js
@@ -0,0 +1,11 @@
+// // VENUE SUGGESTIONS MODEL
+// const mongoose = require("mongoose");
+//
+// const venueSuggestionsSchema = new mongoose.Schema({
+//   userId: {type: mongoose.Types.ObjectId, ref: 'user', required: true},
+//   venueId: {type: mongoose.Types.ObjectId, ref: 'venue', required: true},
+//   suggestion: {type: String, required: true}
+// });
+//
+// const VenueSuggestions = mongoose.model("venueSuggestions", venueSuggestionsSchema, "venueSuggestions");
+// module.exports = VenueSuggestions;
