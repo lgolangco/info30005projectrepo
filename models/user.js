d0a74cea6aa2b05e56f06ef0922a47923e270465
mmdarroch <darrochm@student.unimelb.edu.au>
2020-06-04 13:05:49 +1000

    added admin to user model

--- models/user.js
+++ models/user.js
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema({
   email: {type: String, required: true, unique: true, trim: true},
   password: {type: String, required: true},
   cover: {type: String, default: 'https://tokystorage.s3.amazonaws.com/images/default-cover.png'},
-  avatar: {type: String, default: "https://www.mhcsa.org.au/wp-content/uploads/2016/08/default-non-user-no-photo.jpg"}
+  avatar: {type: String, default: "https://www.mhcsa.org.au/wp-content/uploads/2016/08/default-non-user-no-photo.jpg"},
+  admin: {type: Boolean, default: false}
 });
 
 const User = mongoose.model("user", userSchema, "user");
