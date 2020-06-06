1dfb7100ca68255233e7e46e2f7fceca940f0e08
Su Lyn LOW <lows5@student.unimelb.edu.au>
2020-06-06 15:22:45 +0800

    Made active navbar tab based on current url

---
+++ public/scripts/navbar.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    setActiveTab();
+});
+
+function setActiveTab() {
+    var path = window.location.pathname;
+    console.log(path);
+    if (path.includes("venue")) {
+        $(".venue").addClass("active");
+    } else if (path.includes("user")) {
+        $(".user").addClass("active");
+    } else if (path.includes("register")) {
+        $(".signup").addClass("active");
+    } else if (path.includes("/profile")) {
+        $(".profile").addClass("active");
+    } else if (path.includes("/admin")) {
+        $(".admin").addClass("active");
+    } else if (path.includes("/")) {
+        $(".about").addClass("active");
+    }
+}
